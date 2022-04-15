c4cf785aef6b785a01971bb1917417325d101450
josealiaslloret <jmalias@cash-converters.es>
2022-04-15 23:50:31 +0200

    feat : 🟥 creo test unitario del repository para persistir un cliente.

---
+++ tests/Unit/Mitra/Clients/Infrastructure/DoctrineClientCreatorRepositoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests\Unit\Mitra\Clients\Infrastructure;
+
+use App\Tests\Unit\Mitra\Clients\Domain\Dto\CreatorClientDtoMother;
+use Doctrine\ORM\EntityManagerInterface;
+use Mitra\Clients\Infrastructure\DoctrineClientCreatorRepository;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class DoctrineClientCreatorRepositoryTest extends TestCase
+{
+    private EntityManagerInterface|MockObject $entityManagerMock;
+
+    protected function setUp(): void
+    {
+        $this->entityManagerMock = $this->createMock(EntityManagerInterface::class);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldPersistClientWithoudAddressInDatabaseAndReturnTrue() : void
+    {
+        //GIVEN
+        $creatorClientDto = CreatorClientDtoMother::random();
+
+        $this->entityManagerMock
+            ->expects(self::once())
+            ->method('persist');
+
+        $repository = new DoctrineClientCreatorRepository($this->entityManagerMock);
+
+        //WHEN
+        $result = $repository->save($creatorClientDto);
+
+        self::assertTrue($result);
+
+    }
+
+    public function shouldPersistClientWithAddressInDatabaseAndReturnTrue() : void {}
+
+
+}
