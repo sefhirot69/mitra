4f0f517105b9f6bc87a1ad76c6f203f221f65a46
josealiaslloret <jmalias@cash-converters.es>
2022-04-23 11:37:05 +0200

    feat : instalamos correos

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller;
 
-use App\Entity\AddressDoctrine;
-use App\Entity\ClientDoctrine;
 use App\Message\SmsNotification;
 use Doctrine\ORM\EntityManagerInterface;
-use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
 
-    public function __construct(private EntityManagerInterface $manager, private MessageBusInterface $bus)
-    {
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private MessageBusInterface $bus,
+        private MailerInterface $mailer
+    ) {
     }
 
     #[Route('/test', name: 'app_test')]
@@ -47,6 +49,17 @@ class TestController extends AbstractController
 //        $this->manager->persist($clientDoctrine);
 //        $this->manager->flush($clientDoctrine);
 
+        $email = (new Email())
+            ->from('josalillo@gmail.com')
+            ->to('josalillo@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+        $this->mailer->send($email);
         $this->bus->dispatch(new SmsNotification('Hi!'));
 
         return $this->json([
